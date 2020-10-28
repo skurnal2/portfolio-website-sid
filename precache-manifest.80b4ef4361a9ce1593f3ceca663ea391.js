5af5e3c20351d67166fccabd9e27edf7ff7e3002
Siddharth Kurnal <skurnal2@studentmail.nait.ca>
2020-10-28 12:41:12 -0600

    Updates

---
+++ precache-manifest.80b4ef4361a9ce1593f3ceca663ea391.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247d985f43c3c59e71bd6e254429c7c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf92ac5ee256b599df76",
+    "url": "/static/css/main.41132293.chunk.css"
+  },
+  {
+    "revision": "c4e468c83f79ae31379c",
+    "url": "/static/js/2.d6f2a138.chunk.js"
+  },
+  {
+    "revision": "94708d60e6619beab8515b3eaa6e076f",
+    "url": "/static/js/2.d6f2a138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf92ac5ee256b599df76",
+    "url": "/static/js/main.55934f7c.chunk.js"
+  },
+  {
+    "revision": "9cf7b019a366a03ad75d",
+    "url": "/static/js/runtime-main.be493966.js"
+  },
+  {
+    "revision": "826224425d7f393beac4cb7ff7230c19",
+    "url": "/static/media/bee-chat.82622442.JPG"
+  },
+  {
+    "revision": "af85d8d61d2c5931b43f91f296e03dc8",
+    "url": "/static/media/color-app.af85d8d6.JPG"
+  },
+  {
+    "revision": "d83ab4055e5965a0119cc236d3e81c50",
+    "url": "/static/media/digital-resume.d83ab405.jpg"
+  },
+  {
+    "revision": "b4c6561d0fb40850ed4581dcb7afa07a",
+    "url": "/static/media/geo-covid.b4c6561d.JPG"
+  },
+  {
+    "revision": "f3eba3375cd9d75a17317116a5039efa",
+    "url": "/static/media/highway_dodge.f3eba337.JPG"
+  },
+  {
+    "revision": "9185815f63839088181a727f41d67d67",
+    "url": "/static/media/la-auto-detail.9185815f.JPG"
+  },
+  {
+    "revision": "99de6c3cae90699d3fe3a0697792dae2",
+    "url": "/static/media/laptops-catalog.99de6c3c.JPG"
+  },
+  {
+    "revision": "d3c6eb1f2f549cd692f7852044feb0c4",
+    "url": "/static/media/old-portfolio.d3c6eb1f.JPG"
+  },
+  {
+    "revision": "c70a45f63520f780f3ec4dc47e9e91fb",
+    "url": "/static/media/portfolio-site.c70a45f6.JPG"
+  },
+  {
+    "revision": "0f1f21c995f6f0256bc3dd57b389cfb5",
+    "url": "/static/media/sunshine-meme.0f1f21c9.gif"
+  },
+  {
+    "revision": "679c7c6db30c5805e16f2beb7513825b",
+    "url": "/static/media/wallify.679c7c6d.JPG"
+  }
+]);
